7201648dc14b8839d9a6d4386bd4eacac0a30879
zaranah <zaranahier@gmail.com>
2022-01-14 13:19:26 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,8 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-  # def new
-  # end
-
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
